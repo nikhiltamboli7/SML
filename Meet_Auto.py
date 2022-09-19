9d2e943161633d33bfa76929356d68333738f71f
nikhiltamboli7 <nikhiltamboli7@gmail.com>
2022-09-19 14:57:40 +0200

    Search for wireless access

--- Meet_Auto.py
+++ Meet_Auto.py
@@ -7,8 +7,26 @@ import sys, os, time
 desired_cap = {
     "platformName": "Android",
     "deviceName": "7989a582",
-    #"app": "C:\meetings-0.1.0-543-release-220324-0919.apk"
+    # "app": "C:\meetings-0.1.0-543-release-220324-0919.apk"
 }
+
+
+# Image resized
+def resize():
+    print("Image resizing...")
+    im = Image.open('C:\\Users\\nikhil.tamboli\\Desktop\\ConnectivityProj\\automation\cropped_img\\screen.png')
+
+    # Setting the points for cropped image
+    left = 155
+    top = 110
+    right = 1800
+    bottom = 925
+
+    im1 = im.crop((left, top, right, bottom))
+
+    im1.save('C:\\Users\\nikhil.tamboli\\Desktop\\ConnectivityProj\\automation\cropped_img\\resized_img.png')
+    print("Resized image is saved")
+
 
 # Imge comaparision function
 def compare_img():
@@ -17,8 +35,9 @@ def compare_img():
         "adb exec-out screencap -p > C:\\Users\\nikhil.tamboli\\Desktop\\ConnectivityProj\\automation\\cropped_img\\screen.png",
         shell=True)
     print("image saved at directory")
-    ref_img = Image.open(r'C:\Users\nikhil.tamboli\Desktop\ConnectivityProj\automation\ref_img\signIn.png')
-    crop_img = Image.open('C:\\Users\\nikhil.tamboli\\Desktop\\ConnectivityProj\\automation\cropped_img\\screen.png')
+    resize()
+    ref_img = Image.open(r'C:\Users\nikhil.tamboli\Desktop\ConnectivityProj\automation\ref_img\lobby.png')
+    crop_img = Image.open('C:\\Users\\nikhil.tamboli\\Desktop\\ConnectivityProj\\automation\cropped_img\\resized_img.png')
 
     if list(crop_img.getdata()) == list(ref_img.getdata()):
         print("Image Identical...Test case PASS")
@@ -32,14 +51,12 @@ driver.implicitly_wait(2000)
 print("Connection Success!...")
 driver.wait_activity('Wait', 2)
 
-
 print("Launching the meeting app...")
 driver.wait_activity('Wait', 3)
 driver.find_element_by_xpath('//android.widget.TextView[@content-desc="Meetings"]').click()  # Launch Meeting
 driver.wait_activity('Wait', 1)
 compare_img()
 driver.wait_activity('Wait', 1)
-
 
 print("Clicking on SignIn...")
 driver.wait_activity('Wait', 3)
@@ -65,11 +82,7 @@ print("Clicking on OK")
 driver.wait_activity('Wait', 3)
 driver.find_element_by_id('se.cevt.profilemanager:id/btn_ok').click()
 
-
 print("===================You are now signed In. Execution completed!===========================")
-
-
-
 
 # Locate element by using Class name
 
